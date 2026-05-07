b0547b12b928207f1f9c93f36879991ad84def68
Jenny Ly <jenny@Jennys-MacBook-Air.local>
2026-05-06 23:48:06 -0700

    zipzip tree

--- zipzip_tree.py
+++ zipzip_tree.py
@@ -3,8 +3,10 @@
 
 from __future__ import annotations
 
+import math
 from typing import TypeVar
 from dataclasses import dataclass
+import random
 
 # any variable annotated with KeyType should use the same type for each tree, and should be comparable.
 
@@ -15,15 +17,25 @@ from dataclasses import dataclass
 KeyType = TypeVar('KeyType')
 ValType = TypeVar('ValType')
 
-@dataclass
+@dataclass(frozen=True, order=True)
 class Rank:
 	geometric_rank: int
 	uniform_rank: int
 
+class TreeNode:
+    def __init__(self, key: KeyType, val: ValType, rank: Rank = None, left=None, right=None):
+        self.key = key
+        self.val = val
+        self.rank = rank
+        self.left = left
+        self.right = right
+
 class ZipZipTree:
 	# ZipZipTree(): constructs the zip-zip tree with a specific capacity.
 	def __init__(self, capacity: int):
-		capacity = capacity
+		self.capacity = capacity
+		self.size = 0
+		self.root = None
 		
 	# get_random_rank(): returns a random node rank, chosen independently from:
 	#           a geometric distribution of mean 1 and,
@@ -31,43 +43,165 @@ class ZipZipTree:
 
 
 	def get_random_rank(self) -> Rank:
-		pass
+		geo_rank = 0
+		rand = random.random()
+
+		while rand < 0.5:
+			geom_rank += 1
+			rand = random.random()
+
+		uni_rank = random.randint(0, int(math.log2(self.capacity) ** 3) - 1)
+		return Rank(geometric_rank=geo_rank, uniform_rank=uni_rank)
 
 	# insert(): inserts item with parameter key, value, and rank into tree.
 	#           if rank is not provided, a random rank should be selected by using get_random_rank().
 
 
 	def insert(self, key: KeyType, val: ValType, rank: Rank = None):
-		pass
+		if self.size == self.capacity:
+			return "Cannot insert. Max will be exceeded"
+		
+		self.size += 1
+
+		if not rank:
+			rank = get_random_rank()
+
+		node = TreeNode(key, val, rank)
+
+		if not self.root:
+			self.root = node
+			return
+
+		cur = self.root
+
+		while cur:
+			if rank > cur.rank:
+				break
+			elif rank <= cur.rank:
+				prev = cur
+				cur = cur.left if key < cur.key else cur.right
+
+		if cur == self.root:
+			if cur.key < key:
+				node.left = cur
+			elif cur.key >= key:
+				node.right = cur
+			self.root = node
+		elif key < prev.key:
+			prev.left = node
+		else:
+			prev.right = node
+
+		if not cur:
+			node.left = None
+			node.right = None
+		else:
+			if key < cur.key:
+				node.right = cur 
+			else:
+				node.left = cur 
+
+		prev = node 
+		
+		while cur:
+			fix = prev
+			if cur.key < key:
+				while cur or cur.key < key:
+					prev = cur
+					cur = cur.right
+					is_right = True
+			else:
+				while cur or cur.key > key:
+					prev = cur
+					cur = cur.left
+					is_right = False
+			'''
+			this doesn't make sense to me because we set fix <- prev <- node...
+			if fix.key > key or (fix == node and prev.key > key):
+				fix.left = cur
+			'''
+			if prev.key > key:
+				fix.left = cur
+			else:
+				fix.right = cur
+
+			if is_right:
+				prev.right = None
+			else:
+				prev.left = None
+
+		return
+	
+	# search(): returns the node being searched 
+	def search(self, key: KeyType):
+		cur = self.root
+		while cur.key != key: 
+			if cur.key > key:
+				cur = cur.left
+			elif cur.key < key:
+				cur = cur.right
+			else:
+				break
+
+		return cur
 
 	# remove(): removes item with parameter key from tree.
 	#           you can assume that the item exists in the tree.
 
-
 	def remove(self, key: KeyType):
-		pass
+		node = self.search(key)
+		if node.left:
+			left = node.left
+		if node.right:
+			right = node.right
+
+		
+
+
+	
 
 	# find(): returns the value of item with parameter key.
 	#         you can assume that the item exists in the tree.
 
 	def find(self, key: KeyType) -> ValType:
-		pass
+		if not self.root:
+			return None
+		
+		cur = self.root
+
+		while cur:
+			if cur.key < key:
+				cur = cur.right
+			elif cur.key >= key:
+				cur = cur.left
+
+		
 
 	# get_size(): returns the number of nodes in the tree.
 
 	def get_size(self) -> int:
-		pass
+		return self.size
+		
 
 	# get_height(): returns the height of the tree.
 
 	def get_height(self) -> int:
-		pass
+		if not self.root:
+			return -1
+		
+		cur = self.root
+		height = 0
+
+		while cur:
+			height += 1
+
+
 
 	# get_depth(): returns the depth of the item with parameter key.
 	#              you can assume that the item exists in the tree.
 
 	def get_depth(self, key: KeyType):
-		pass
+		
 
 	# feel free to define new methods in addition to the above
 	# fill in the definitions of each required member function (above),
