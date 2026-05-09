2dd40e3dbb90f32f9c788822f722c7db913fd3c1
Jenny Ly <jtly102@gmail.com>
2026-05-09 16:51:26 -0700

    original nf????

--- next_fit.py
+++ next_fit.py
@@ -83,7 +83,7 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 
 
 # Original: 6/30
-
+'''
 def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
 	bin_count = 0
 	cur_capacity = 0
@@ -110,7 +110,7 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 		free_space.append(remaining_capacity)
 
 	return free_space
-'''
+
 items = [0.7, 0.5, 0.2, 0.1, 0.2, 0.9, 0.3]
 items = [0.79, 0.88, 0.95, 0.12, 0.05, 0.46, 0.53, 0.64, 0.04, 0.38, 0.03, 0.26]
 
