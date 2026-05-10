e100ae4d3ebf7b1573d9e90ab2530b7a451e7a34
Jenny Ly <jtly102@gmail.com>
2026-05-10 13:39:30 -0700

    adjusted fp handling in nf

--- next_fit.py
+++ next_fit.py
@@ -49,31 +49,31 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 
 	return free_space
 '''
-'''
-def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
-	bin_count = 0
-	cur_capacity = 0
-	
-	SCALE = 1e-7
-	remaining_capacity = SCALE
 
+def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
+	if not items:
+		return
+	
+	bin_count = 0	
+	SCALE = 1e-9
+	remaining_capacity = 1
+	'''
 	if not items:
 		free_space.append(remaining_capacity)
 		return free_space
+	'''
 
 	bins = {bin_count: []}
 
 	for i in range(len(items)):
-		if cur_capacity >= 1 or (items[i] > remaining_capacity + SCALE):
+		if items[i] > remaining_capacity + SCALE:
 			free_space.append(remaining_capacity)
 			bin_count += 1
 			bins[bin_count] = []
-			cur_capacity = 0
-			remaining_capacity = SCALE
+			remaining_capacity = 1.0
 
 
 		bins[bin_count].append(items[i])
-		cur_capacity += items[i]
 		remaining_capacity -= items[i]
 		assignment[i] = bin_count
 
@@ -86,6 +86,7 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 
 
 # Original: 6/30
+
 '''
 def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
 	bin_count = 0
@@ -113,6 +114,7 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 		free_space.append(remaining_capacity)
 
 	return free_space
+'''
 
 items = [0.7, 0.5, 0.2, 0.1, 0.2, 0.9, 0.3]
 items = [0.79, 0.88, 0.95, 0.12, 0.05, 0.46, 0.53, 0.64, 0.04, 0.38, 0.03, 0.26]
